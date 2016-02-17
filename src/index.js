6a7ab00f53652cfad3e52ac9f5846a171c8ab17a
lgoda <luca.godano@gmail.com>
2016-02-17 23:04:20 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,54 @@
+import _ from 'lodash'
+import React, { Component} from 'react';
+import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_details';
+
+const API_KEY = 'AIzaSyAM8U_GoE3IoO_hwIg99B8_ah6ovY5lUSk';
+
+
+
+//Create a new component. This component should produce
+// some HTML
+/*const App = () => {
+   return (<div>
+      <SearchBar />
+   </div>);
+}*/
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
+    this.videoSearch('surfBoards');
+  }
+  videoSearch(term) {
+    YTSearch({key: API_KEY, term: term}, (videos) => {
+      //this.setState({ videos: videos });
+      //this.setState({ videos });
+      this.setState({
+        videos:videos,
+        selectedVideo: videos[0]
+      });
+    });
+  }
+  render() {
+    const videoSearch = _.debounce((term) => { this.videoSearch(term)}, 300);
+     return (
+       <div>
+          <SearchBar onSearchTermChange={videoSearch} />
+          <VideoDetail video={this.state.selectedVideo} />
+          <VideoList onVideoSelect={selectedVideo => this.setState({selectedVideo})} videos={this.state.videos} />
+       </div>);
+  }
+}
+
+
+// Thake this component's generated HTML and put it
+// on the page (in the DOM)
+ReactDOM.render(<App />, document.querySelector('.container'));
